b0bef088504342e10cc28a9c773aceec8b4e6442
genjins <genjins@gmail.com>
2022-05-17 16:30:51 +0900

    .

--- Baekjoon/java/exam/src/exam1000/Main.java
+++ Baekjoon/java/exam/src/exam1000/Main.java
@@ -3,6 +3,9 @@ package exam1000;
 import java.util.Scanner;
 
 public class Main {
+  /**
+   * The function takes two integers as input, adds them together, and prints the result to the console
+   */
    public static void main(String[] args) {
        Scanner scan = new Scanner(System.in);
 
--- Baekjoon/java/exam/src/exam2292/Main.java
+++ Baekjoon/java/exam/src/exam2292/Main.java
@@ -6,6 +6,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+   /**
+    * It's a function that takes in a number and returns the number of rooms in the building that has
+    * that number of rooms
+    */
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
